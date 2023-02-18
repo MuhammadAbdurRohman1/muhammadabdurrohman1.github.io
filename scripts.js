6aeaf5ef87bbc97f9d6fcc7d27d5ccf5225acb36
MuhammadAbdurRohman1 <tarnojepara6@gmail.com>
2023-02-18 16:35:23 +0700

    y

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-const sidenav = document.getElementsByClassName("sidenav");
-function navExpand(){
-    sidenav.style.width = "250px";
-}
+const body = document.body;
+const toogle = document.querySelector('.navtoogle');
+const nav = document.querySelector('.sidenav');
+console.log(body);
+toogle.addEventListener("click" , myFunction);
+
+
+function myFunction() {
+    if (nav.style.right == "-100%") {
+        nav.style.right = "0";
+    } else {
+        nav.style.right = "-100%";
+    }
+  }
