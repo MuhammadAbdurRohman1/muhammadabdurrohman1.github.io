2b7103444cd289897ab0d90a6e5f52c952db88ee
Tukiman <70331295+MuhammadAbdurRohman1@users.noreply.github.com>
2023-02-21 17:36:29 +0700

    vv

--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
 const body = document.body;
 const toogle = document.querySelector(".navtoogle");
 const nav = document.querySelector(".sidenav");
-var right = nav.style.right;
 console.log(right);
 toogle.addEventListener("click", myFunction);
 
 function myFunction() {
-  if (right == "-100%") {
-    right = "0%";
-  } else {
-    right = "-100%";
-  } 
+  nav.classList.remove ("hidden");
+  nav.classList.add ("expand");
 }
